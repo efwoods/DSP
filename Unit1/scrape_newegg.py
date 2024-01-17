7ccee0d079b88b5935994eef26e8b4893a56ff1f
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-01-17 15:57:32 -0500

    update

--- Unit1/scrape_newegg.py
+++ Unit1/scrape_newegg.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-my_url = input("Url to scrape:")
+# my_url = input("Url to scrape:")
+my_url = "https://www.newegg.com/p/pl?d=playstation"
 print("Grabbing...", my_url)
 
 response = requests.get(my_url)
@@ -15,8 +16,15 @@ else:
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
     
+
     items_ = soup.find_all("div", attrs={"class":"item-cell"})
     for i in range(len(items_)):
         item = items_[i]
-        print(item.text)
+        
+        title = item.find("a", attrs={"class":"item-title"}).text
+        price = item.find("li", attrs={"class":"price-current"})
+        price = float(price.find("strong").text.replace(',', '') + price.find("sup").text)
+        
+        print("Title: ", title, "\nPrice: ", price)
     
+
